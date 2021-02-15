7a00796417871c787d146c4f7846e0ab3f115d1a
rheadp <rheadpea@msu.edu>
2021-02-15 11:47:13 -0500

    commit tree 89340a4040dc3e4c501d41eeffc1f5f97f9e54eb

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,30 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+holiday_hash[:summer][:fourth_of_july][1]
+
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+holiday_hash[:winter].each do |key, value|
+  holiday_hash[:winter][key] << supply
+end
 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+holiday_hash[:spring].each do |key, value|
+  holiday_hash[:spring][key] << supply
+end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,15 +52,35 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, data|
+  puts "#{season.to_s.capitalize!}:"
+  data.each do |holiday, supply|
+    a = holiday.to_s.split("_")
+    holiday_update = []
+    a.each do |i|
+      holiday_update << i.capitalize!
+    end
+    holiday = holiday_update.join(" ")
+    supply = supply.join(", ")
+    puts "  #{holiday}: #{supply}"
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+array = []
+holiday_hash.each do |season, data|
+  data.each do |holiday, value|
+    value.each do |supply|
+      if supply == "BBQ"
+        array.push(holiday)
+      end
+    end
+  end
+end
+array
 
 end
-
 
 
 
